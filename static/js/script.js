db7b901340e74aed9af024c6da6cde54ec416ac2
bvthien1722 <5101103084@student.hcmue.edu.vn>
2026-05-01 23:31:38 +0700

    1.7.2

--- static/js/script.js
+++ static/js/script.js
@@ -11,4 +11,34 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }, 3000);
+
+    // Dọn dẹp video khi đóng Modal để tiết kiệm băng thông và RAM
+    const videoModalEl = document.getElementById('videoModal');
+    if(videoModalEl) {
+        videoModalEl.addEventListener('hidden.bs.modal', function () {
+            const videoEl = document.getElementById('previewVideo');
+            videoEl.pause();
+            videoEl.src = ""; 
+            videoEl.load();
+        });
+    }
 });
+
+/**
+ * Mở Media Viewer (Hình ảnh/Video)
+ */
+function openMediaViewer(type, fileId, name, size, date) {
+    if (type === 'image') {
+        document.getElementById('previewImage').src = `/download/${fileId}`;
+        document.getElementById('imgName').innerText = name;
+        document.getElementById('imgSize').innerText = size;
+        document.getElementById('imgDate').innerText = date;
+        new bootstrap.Modal(document.getElementById('imageModal')).show();
+    } else if (type === 'video') {
+        const videoEl = document.getElementById('previewVideo');
+        videoEl.src = `/stream/${fileId}`; // Trỏ đến API stream video
+        document.getElementById('videoName').innerText = name;
+        new bootstrap.Modal(document.getElementById('videoModal')).show();
+        videoEl.play();
+    }
+}
